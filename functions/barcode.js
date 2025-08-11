5edc2ebdddccec8c44eebe1997519ba2454703c3
robertagelo <123727348+robertagelo@users.noreply.github.com>
2025-08-11 16:38:42 +0200

    Added canvas to add more style to the barcode

--- functions/barcode.js
+++ functions/barcode.js
@@ -1,23 +1,45 @@
 const bwipjs = require("bwip-js");
+const { createCanvas, loadImage } = require("canvas");
 
 exports.handler = async (event) => {
-  const data = event.queryStringParameters.data || "123456789";
+  const data = event.queryStringParameters.data || "12345678";
 
   try {
-    const png = await bwipjs.toBuffer({
+    // Per generare le barre di testo
+    const barcodeBuffer = await bwipjs.toBuffer({
       bcid: "code128",
       text: data,
       scale: 3,
       height: 10,
       includetext: false,
+      paddingwidth: 20,
+      paddingheight: 20,
     });
 
+    // Per il testo
+    const barcodeImage = await loadImage(barcodeBuffer);
+    const marginBottom = 30;
+    const canvas = createCanvas(
+      barcodeImage.width,
+      barcodeImage.height + marginBottom
+    );
+    const ctx = canvas.getContext("2d");
+
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.drawImage(barcodeImage, 0, 0);
+
+    ctx.font = "20px Arial";
+    ctx.fillStyle = "black";
+    ctx.textAlign = "center";
+    ctx.fillText(data, canvas.width / 2, barcodeImage.height + 20);
+
+    const finalBuffer = canvas.toBuffer("image/png");
     return {
       statusCode: 200,
-      headers: {
-        "Content-Type": "image/png",
-      },
-      body: png.toString("base64"),
+      headers: { "Content-Type": "image/png" },
+      body: finalBuffer.toString("base64"),
       isBase64Encoded: true,
     };
   } catch (err) {
