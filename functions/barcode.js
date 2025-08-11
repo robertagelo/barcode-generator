97085caa168f9458b0b111230d49363b33746021
robertagelo <123727348+robertagelo@users.noreply.github.com>
2025-08-11 16:55:29 +0200

    Rollback

--- functions/barcode.js
+++ functions/barcode.js
@@ -1,51 +1,22 @@
 const bwipjs = require("bwip-js");
-const { createCanvas, loadImage } = require("canvas");
 
-exports.handler = async (event) => {
-  const data = event.queryStringParameters.data || "123456789";
-
-  try {
-    // Per generare le barre di testo
-    const barcodeBuffer = await bwipjs.toBuffer({
-      bcid: "code128",
+app.get("/barcode", (req, res) => {
+  const data = req.query.data || "123456789";
+  bwipjs.toBuffer(
+    {
+      bcid: "code128", // oppure 'ean13' ecc.
       text: data,
       scale: 3,
       height: 10,
       includetext: false,
-      paddingwidth: 5,
-      paddingheight: 5,
-    });
-
-    // Per il testo
-    const barcodeImage = await loadImage(barcodeBuffer);
-    const marginBottom = 10;
-    const canvas = createCanvas(
-      barcodeImage.width,
-      barcodeImage.height + marginBottom
-    );
-    const ctx = canvas.getContext("2d");
-
-    ctx.fillStyle = "white";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-    ctx.drawImage(barcodeImage, 0, 0);
-
-    ctx.font = "20px Arial";
-    ctx.fillStyle = "black";
-    ctx.textAlign = "center";
-    ctx.fillText(data, canvas.width / 2, barcodeImage.height + 20);
-
-    const finalBuffer = canvas.toBuffer("image/png");
-    return {
-      statusCode: 200,
-      headers: { "Content-Type": "image/png" },
-      body: finalBuffer.toString("base64"),
-      isBase64Encoded: true,
-    };
-  } catch (err) {
-    return {
-      statusCode: 500,
-      body: `Errore: ${err.message}`,
-    };
-  }
-};
+    },
+    function (err, png) {
+      if (err) {
+        res.status(400).send("Errore nel generare il barcode");
+      } else {
+        res.set("Content-Type", "image/png");
+        res.send(png);
+      }
+    }
+  );
+});
